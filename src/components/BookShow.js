0dfaa680997f32940cde625f795239bb6ba2b91f
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2023-10-20 13:56:12 -0700

    finishing touches on edit form, adding images

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import BookEdit from './BookEdit'
 
-function BookShow({ book, onDelete }) {
+function BookShow({ book, onEdit, onDelete }) {
   const [showEdit, setShowEdit] = useState(false)
 
   const handleEditClick = () => {
@@ -11,13 +11,22 @@ function BookShow({ book, onDelete }) {
     onDelete(book.id)
   }
 
+  const handleSubmit = (id, newTitle) => {
+    setShowEdit(false)
+    onEdit(id, newTitle)
+  }
+
   let content = <h3>{book.title}</h3>
   if (showEdit) {
-    content = <BookEdit book={book}/>
+    content = <BookEdit onSubmit={handleSubmit} book={book}/>
   }
 
   return (
     <div className="book-show">
+      <img
+        alt="books"
+        src={`https://picsum.photos/seed/${book.id}/300/200`}
+      />
       <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
